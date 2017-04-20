2a4994be2191f816ed6626aa86420ac48805ca2b
Junjun Zhang <junjun.ca@gmail.com>
2017-04-20 12:48:37 -0400

    handles input argument in command line as json string

--- tasks/download.py
+++ tasks/download.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+from utils import get_task_dict
 import json
+
+task_dict = get_task_dict(argv[1])
+
+
+with open('task_json.json', 'w') as f:
+  f.write(json.dumps(task_dict))
+
 
 with open('output.json', 'w') as f:
   f.write(json.dumps({"task": "download"}))
