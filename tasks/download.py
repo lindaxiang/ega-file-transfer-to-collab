a23a871700981326a8241e80cbbb972ead0f5393
lindaxiang <linda.xiang@oicr.on.ca>
2017-04-24 17:11:12 -0400

    test download

--- tasks/download.py
+++ tasks/download.py
@@ -6,7 +6,9 @@ import sys
 
 task_dict = get_task_dict(sys.argv[1])
 
-
+for f in task_dict.get('input').get('files'):
+	echo 'this is a test file' > f.get('file_name')
+	
 with open('task_json.json', 'w') as f:
   f.write(json.dumps(task_dict))
 
